bd8796364b4377e3a02bd69c54a20eb2b3ee02a9
Mohammad Faisal <mfaisalcs2017@gmail.com>
2025-12-04 11:40:59 +0300

    sqlite to postgres

--- prisma/migrations/20251204084006_init/migration.sql
+++ prisma/migrations/20251204084006_init/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE "Snippet" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "code" TEXT NOT NULL DEFAULT '',
     "language" TEXT NOT NULL DEFAULT 'javascript',
     "protected" BOOLEAN NOT NULL DEFAULT false,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Snippet_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
